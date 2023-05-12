3d5ba8ff32957f7b90227efda5e04b446bd830cd
brescou <brescou.thomas@gmail.com>
2023-05-12 17:13:34 -0400

    add logging info

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from pymongo import MongoClient
 from flask_jwt_extended import JWTManager
 from .api.v1 import v1_blueprint
+import logging
+
 
 def create_app(config_object):
     app = Flask(__name__)
@@ -11,8 +13,19 @@ def create_app(config_object):
     global mongo
     mongo = MongoClient(app.config["MONGO_URI"])
     jwt.init_app(app)
+    configure_logging(app)
 
     return app
+
+
+def configure_logging(app):
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter(
+        '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+    ))
+    app.logger.addHandler(handler)
+    app.logger.setLevel(app.config['LOG_LEVEL'])
+
 
 mongo = None
 jwt = JWTManager()
