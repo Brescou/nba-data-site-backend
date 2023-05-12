cd124b84e0e0f796160be843bb6a674fcbbff484
brescou <brescou.thomas@gmail.com>
2023-05-12 17:26:37 -0400

    add user model

---
+++ src/app/model/user.py
@@ -0,0 +1,17 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from .base import MongoModel
+
+class User(MongoModel):
+    collection_name = 'users'
+
+    @classmethod
+    def create(cls, username, password):
+        user = {"username": username, "password": generate_password_hash(password)}
+        cls.insert(user)
+
+    @property
+    def username(self):
+        return self.document["username"]
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
