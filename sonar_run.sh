82bb690a83741e641c9a7dd334c8daed4d8b294a
brescou <brescou.thomas@gmail.com>
2023-05-13 11:46:51 -0400

    sh for run SQ locally

---
+++ sonar_run.sh
@@ -0,0 +1,5 @@
+sonar-scanner \
+  -Dsonar.projectKey=nba-data-site-backend \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.token=sqp_90f6eaa2c364509ff9830a997303a62a3843ad2c
