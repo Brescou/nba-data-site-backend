b374f02c476619353eb47b2dac8cca41ff6180ba
brescou <brescou.thomas@gmail.com>
2023-05-13 11:45:51 -0400

    config prod

--- app/configs/production.py
+++ app/configs/production.py
@@ -1,19 +1,22 @@
+import datetime
+
+from decouple import config
+
 DB_CONNECTION = {
-    "MONGODB_DB": "",
-    "MONGODB_USERNAME": "",
-    "MONGODB_PASSWORD": "",
-    "MONGODB_HOST": "",
-    "MONGODB_PORT": None
+    "MONGODB_DB": config('DB_NAME_DEV'),
+    "MONGODB_USERNAME": config('DB_USER_DEV'),
+    "MONGODB_PASSWORD": config('DB_PASSWORD_DEV'),
+    "MONGODB_HOST": config('MONGODB_HOST'),
+    "MONGODB_PORT": 27017,
+    "MONGODB_URI": f"mongodb+srv://{config('DB_USER')}:{config('DB_PASSWORD')}@cluster-uni-work.axy4lk0" \
+                   f".mongodb.net/{config('DB_NAME_DEV')}?retryWrites=true&w=majority"
 }
 
-DATABASE_URI = ''
-
 FLASK_VARS = {
-    'SECRET_KEY': '',
+    'SECRET_KEY': 'SecretKey',
 }
 
 FLASK_JWT_VARS = {
     'JWT_AUTH_URL_RULE': '/api/auth',
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7)
 }
-# MONGO_URI = f"mongodb+srv://{config('DB_USER')}:{config('DB_PASSWORD')}@cluster-uni-work.axy4lk0.mongodb" \
-#             f".net/{config('DB_NAME_DEV')}?retryWrites=true&w=majority"
