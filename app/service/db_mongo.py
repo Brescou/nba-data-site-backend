2a0418689acffb395b8b6e9720bc089e5719cfff
brescou <brescou.thomas@gmail.com>
2023-05-30 19:30:34 -0400

    fix connection db_mongo.py

--- app/service/db_mongo.py
+++ app/service/db_mongo.py
@@ -2,21 +2,28 @@ import flask
 from flask import current_app
 from pymongo import MongoClient
 
-
 class Mongo:
     def __init__(self, app=None):
+        self._mongo = None
         if app is not None:
             self.init_app(app)
 
     def init_app(self, app):
         app.teardown_appcontext(self.teardown)
+        app.mongo = self
 
-    def connect(self):
-        if 'mongo' not in flask._app_ctx_stack.top:
-            flask._app_ctx_stack.top.mongo = MongoClient(current_app.config['MONGODB_URI'])
-        return flask._app_ctx_stack.top.mongo
+    @property
+    def client(self):
+        ctx = flask._app_ctx_stack.top
+        if ctx is not None:
+            if not hasattr(ctx, 'mongo'):
+                ctx.mongo = MongoClient(current_app.config['MONGODB_URI'])
+                ctx.db = ctx.mongo[current_app.config['MONGODB_DB']]
+            return ctx.db
+        else:
+            raise RuntimeError('Outside of application context.')
 
     def teardown(self, exception):
-        mongo = getattr(flask._app_ctx_stack.top, 'mongo', None)
-        if mongo is not None:
-            mongo.close()
+        ctx = flask._app_ctx_stack.top
+        if ctx is not None and hasattr(ctx, 'mongo'):
+            ctx.mongo.close()
